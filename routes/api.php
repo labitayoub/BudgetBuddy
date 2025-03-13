1ebccbc511ce79a8a5db02b1315f01d5bb4af770
labitayoub <ayoub.labite@gmail.com>
2025-03-13 03:12:17 +0000

    update

--- routes/api.php
+++ routes/api.php
@@ -17,35 +17,30 @@ use App\Http\Controllers\Api\AuthController;
 */
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     //     return $request->user();
+    
+    Route::post('/auth/register', [AuthController::class, 'createUser']);
+    Route::post('/auth/login', [AuthController::class, 'loginUser']);
+    
+    Route::post('auth/logout', [AuthController::class, 'logoutUser']);
+    
+    // Route::middleware('auth:sanctum')->group(function () {
+        //     Route::get('/user', function (Request $request) {
+            //         return $request->user();
+            //     });
+            // });
+            
+            // Route::resource('expenses', ExpenseController::class);
+            
+            Route::get('/expenses/search/{name}', [ExpenseController::class, 'search']);
+            Route::get('/expenses', [ExpenseController::class, 'index']);
+            Route::get('/expenses{id}', [ExpenseController::class, 'show']);
+            
+            
+            Route::group(['middleware' => ['auth:sanctum']], function () {
+                Route::post('/expenses', [ExpenseController::class, 'store']);
+                Route::put('/expenses/{id}', [ExpenseController::class, 'update']);
+                Route::delete('/expenses', [ExpenseController::class, 'destroy']);
+                //     Route::post('/logout', [AuthController::class, 'logout']);
+                });
+                
 
-Route::post('/auth/register', [AuthController::class, 'createUser']);
-Route::post('/auth/login', [AuthController::class, 'loginUser']);
-
-Route::post('auth/logout', [AuthController::class, 'logoutUser']);
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::get('/user', function (Request $request) {
-//         return $request->user();
-//     });
-// });
-
-Route::resource('expenses', ExpenseController::class);
-
-
-Route::get('/expenses/search/{name}', [ExpenseController::class, 'search']);
-// Route::get('/expenses', [ExpenseController::class, 'index']);
-// Route::post('/expenses', [ExpenseController::class, 'store']);
-// Route::get('/expenses{id}', [ExpenseController::class, 'show']);
-// Route::post('/expenses', [ExpenseController::class, 'update']);
-
-// Route::group(['middleware' => ['auth:sanctum']], function () {
-//     Route::post('/expenses', [ExpenseController::class, 'store']);
-//     Route::put('/expenses/{id}', [ExpenseController::class, 'update']);
-//     Route::delete('/expenses/{id}', [ExpenseController::class, 'destroy']);
-//     Route::post('/logout', [AuthController::class, 'logout']);
-// });
-
-
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
