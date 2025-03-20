207943a0a20bb662ef79ad2c53c14b6f8286736d
labitayoub <ayoub.labite@gmail.com>
2025-03-20 02:34:01 +0000

    udpate Models

--- app/Http/Controllers/GroupUserController.php
+++ app/Http/Controllers/GroupUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\GroupUser;
 
 class GroupUserController extends Controller
 {
@@ -13,7 +14,8 @@ class GroupUserController extends Controller
      */
     public function index()
     {
-        //
+        $data = GroupUser::all();
+        return response()->json([$data], 201);
     }
 
     /**
